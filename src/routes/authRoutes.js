cf4d510d6713aa13e58a44e4c11e2a214e0a5d96
aadiix23 <aadiix23@gmail.com>
2025-11-28 13:48:19 +0530

    a

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser } = require("../controllers/authcontroller");
+
+const { registerUser, loginUser } = require("../controllers/authcontroller");  
+// NOTE: Capital C in authController
 
 router.post("/signup", registerUser);
+router.post("/login", loginUser);
 
 module.exports = router;
